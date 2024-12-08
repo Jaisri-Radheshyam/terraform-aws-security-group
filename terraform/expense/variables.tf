4f1559d2b23029264afe4508ef8080dfd5ffd397
jayasris <saluva.jayasri@gmail.com>
2024-12-08 17:15:03 +0530

    terraform

---
+++ terraform/expense/variables.tf
@@ -0,0 +1,21 @@
+variable "instance_names" {
+    type = list(string)
+    default = ["mysql", "backend", "frontend"]
+}
+
+variable "common_tags" {
+    type = map
+    default = {
+        Project = "expense"
+        Environment = "dev"
+        Terraform = "true"
+    }
+}
+
+variable "zone_id" {
+    default = "Z054018710GBU65EBWU93"
+}
+
+variable "domain_name" {
+    default = "daws-81s.fun"
+}
